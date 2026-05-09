5b582fe786ff72e0bc18dea67bec27eb5714f843
unknown <adelinatimosco16@gmail.com>
2026-05-09 16:55:56 +0300

    initial commit

---
+++ app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = (
+    "mssql+pyodbc://localhost/Dummy"
+    "?driver=ODBC+Driver+17+for+SQL+Server"
+    "&trusted_connection=yes"
+)
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
